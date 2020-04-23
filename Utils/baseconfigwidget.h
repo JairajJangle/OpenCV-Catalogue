2d014cc7f5507eff875976d7d8dc77b28421fa69
JairajJangle <jairaj.jangle@gmail.com>
2020-04-23 19:45:37 +0530

    Created initWidget function to avoid repititons, derived class should call this function with Base class namespace

--- Utils/baseconfigwidget.h
+++ Utils/baseconfigwidget.h
@@ -6,6 +6,17 @@
 #include <QHBoxLayout>
 #include <opencv2/opencv.hpp>
 
+// QT libs
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QCheckBox>
+#include <QRadioButton>
+#include <QScrollArea>
+#include <QLabel>
+#include <QThread>
+#include <QSizePolicy>
+#include <QSpacerItem>
+
 #include "CustomWidgets/ClickableLabel/clickablelabel.h"
 
 using namespace cv;
@@ -17,9 +28,21 @@ protected:
     QString operationName = "No operation Selected";
     QString moreInfoLink = "";
     ClickableLabel* labelInfo = new ClickableLabel();
+    Point begin;
+    Point end;
+
+    QScrollArea *scrl = new QScrollArea();
+
+    QWidget *wgtMain = new QWidget();
+    QWidget *wgtSub = new QWidget();
+
+    QVBoxLayout *vboxMain = new QVBoxLayout(wgtMain);
+    QVBoxLayout *vBoxSub = new QVBoxLayout(wgtSub);
 
 public:
     BaseConfigWidget(){
+        begin = Point(-1, -1);
+        end = Point(-1, -1);
         labelInfo->setText("More Info >>");
     }
     ~BaseConfigWidget(){}
@@ -41,6 +64,13 @@ public:
 
     virtual Mat getProcessedImage(Mat inputImage){
         return inputImage;
+    }
+
+    virtual void initWidget()
+    {
+        vboxMain->addWidget(wgtSub);
+        scrl->setWidget(wgtMain);
+        configWidget = scrl;
     }
 };
 
