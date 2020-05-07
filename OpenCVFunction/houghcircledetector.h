d5550cd481029566caf19957e7fb329df2e0c6c2
JairajJangle <jairaj.jangle@gmail.com>
2020-05-07 19:13:23 +0530

    Added constructor and class for Hough Circle operation

--- OpenCVFunction/houghcircledetector.h
+++ OpenCVFunction/houghcircledetector.h
@@ -1,4 +1,27 @@
 #ifndef HOUGHCIRCLEDETECTOR_H
 #define HOUGHCIRCLEDETECTOR_H
 
+/*
+ * @author JairajJangle
+ * @email jairaj.jangle@gmail.com
+ */
+
+// QT libs
+#include <QRadioButton>
+
+#include "Utils/baseconfigwidget.h"
+
+class HoughCircleDetector: public QWidget, public BaseConfigWidget
+{
+    Q_OBJECT
+
+public:
+    HoughCircleDetector()
+    {
+        operationName = "Hough Circle Detector";
+        moreInfoLink = "https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html";
+        initWidget();
+    }
+};
+
 #endif // HOUGHCIRCLEDETECTOR_H
