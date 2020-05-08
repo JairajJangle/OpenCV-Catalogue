3e889fe8012525cde601e435c9e7b14ccbf44fb1
JairajJangle <jairaj.jangle@gmail.com>
2020-05-08 20:35:46 +0530

    Added Blur checkbox to Hough Circle operation class

--- OpenCVFunction/houghcircledetector.h
+++ OpenCVFunction/houghcircledetector.h
@@ -8,6 +8,7 @@
 
 // QT libs
 #include <QRadioButton>
+#include <QCheckBox>
 
 #include "Utils/baseconfigwidget.h"
 
@@ -19,9 +20,34 @@ public:
     HoughCircleDetector()
     {
         operationName = "Hough Circle Detector";
-        moreInfoLink = "https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html";
+        moreInfoLink = "https://docs.opencv.org/2.4/modules/imgproc/doc/feature_detection.html?highlight=houghcircles#houghcircles";
         initWidget();
     }
+
+    cv::Mat getProcessedImage(cv::Mat inputImage)
+    {
+        cv::Mat outputImage;
+
+        if(enableBlurCB->isChecked())
+            blur( inputImage, outputImage, cv::Size(3,3));
+
+        return inputImage;
+    }
+
+    ~HoughCircleDetector()
+    {
+        printf("Hough Circle destroyed\n");
+    }
+
+private:
+    QCheckBox* enableBlurCB = new QCheckBox("Enable Blur");
+
+    void initWidget()
+    {
+        vBoxSub->addWidget(enableBlurCB);
+        BaseConfigWidget::initWidget();
+    }
+
 };
 
 #endif // HOUGHCIRCLEDETECTOR_H
