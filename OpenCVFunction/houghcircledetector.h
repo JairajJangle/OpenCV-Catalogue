b31b0b872b8fc5fbc15e611c3777bea4e98ac758
JairajJangle <jairaj.jangle@gmail.com>
2020-05-08 20:56:33 +0530

    Added non customizable Hough circle detection for testing.

--- OpenCVFunction/houghcircledetector.h
+++ OpenCVFunction/houghcircledetector.h
@@ -26,10 +26,32 @@ public:
 
     cv::Mat getProcessedImage(cv::Mat inputImage)
     {
-        cv::Mat outputImage;
+        cv::Mat grayImage;
+
+        cvtColor(inputImage, grayImage, CV_BGR2GRAY);
 
         if(enableBlurCB->isChecked())
-            blur( inputImage, outputImage, cv::Size(3,3));
+            blur(inputImage, grayImage, cv::Size(3,3));
+
+        std::vector<cv::Vec3f> circles;
+
+        /// Apply the Hough Transform to find the circles
+        HoughCircles(grayImage, circles, CV_HOUGH_GRADIENT,
+                     1, grayImage.rows/8, 200, 100, 0, 0 );
+
+        /// Draw the circles detected
+        for( size_t i = 0; i < circles.size(); i++ )
+        {
+            cv::Point center(cvRound(circles[i][0]),
+                    cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            // circle center
+            circle(inputImage, center, 3,
+                   cv::Scalar(0,255,0), -1, 8, 0 );
+            // circle outline
+            circle(inputImage, center, radius,
+                   cv::Scalar(0,0,255), 3, 8, 0 );
+        }
 
         return inputImage;
     }
