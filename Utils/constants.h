204c2042322ce545e070ef4ba928773ea93e99e8
JairajJangle <jairaj.jangle@gmail.com>
2020-05-10 19:32:09 +0530

    Removed unused regexes

--- Utils/constants.h
+++ Utils/constants.h
@@ -4,7 +4,6 @@
 namespace RegExps {
     #include <QRegExp>
      const QRegExp regEx0_1Decimal = QRegExp("^(0(\\.[0-9]{1,4})?|1(\\.0{1,4})?)$");
-     const QRegExp regExInt_greaterThan0 = QRegExp("^[1-9][0-9]*$");
 }
 
 #define GET_VARIABLE_NAME(Variable) (#Variable)
