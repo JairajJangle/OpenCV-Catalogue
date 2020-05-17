bcbfa2444bcd8ec180035694cde10f031873897f
JairajJangle <jairaj.jangle@gmail.com>
2020-05-17 09:56:18 +0530

    Clean up

--- OpenCVFunction/histogram.h
+++ OpenCVFunction/histogram.h
@@ -28,41 +28,41 @@ public:
     cv::Mat getProcessedImage(cv::Mat inputImage)
     {
         std::vector<cv::Mat> bgr_planes;
-        split( inputImage, bgr_planes );
+        split(inputImage, bgr_planes);
 
         int histSize = 256;
 
-        float range[] = { 0, 256 }; //the upper boundary is exclusive
+        float range[] = {0, 256}; //the upper boundary is exclusive
 
-        const float* histRange = { range };
+        const float* histRange = {range};
 
         bool uniform = true, accumulate = false;
 
         cv::Mat b_hist, g_hist, r_hist;
 
-        calcHist( &bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-        calcHist( &bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-        calcHist( &bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+        calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+        calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+        calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
 
         int hist_w = 512, hist_h = 400;
-        int bin_w = cvRound( (double) hist_w/histSize );
+        int bin_w = cvRound((double)hist_w/histSize);
 
-        cv::Mat histImage( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
-        normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-        normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-        normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+        cv::Mat histImage( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0));
+        normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+        normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+        normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
 
         for(int i = 1; i < histSize; i++)
         {
-            line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ),
-                  cv::Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                  cv::Scalar( 255, 0, 0), 2, 8, 0  );
-            line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ),
-                  cv::Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                  cv::Scalar( 0, 255, 0), 2, 8, 0  );
-            line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ),
-                  cv::Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                  cv::Scalar( 0, 0, 255), 2, 8, 0  );
+            line(histImage, cv::Point(bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1))),
+                 cv::Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))),
+                 cv::Scalar(255, 0, 0), 2, 8, 0);
+            line(histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1))),
+                 cv::Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))),
+                 cv::Scalar(0, 255, 0), 2, 8, 0);
+            line(histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1))),
+                 cv::Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))),
+                 cv::Scalar(0, 0, 255), 2, 8, 0);
         }
 
         return histImage;
