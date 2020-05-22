da435921fae68ab158929205ed1e5b51b250b49c
JairajJangle <jairaj.jangle@gmail.com>
2020-05-22 18:40:29 +0530

    Added Edit Button Modes and replaced value text with Line Edit widget for forcing a value

--- hybridslider.cpp
+++ hybridslider.cpp
@@ -1,11 +1,23 @@
 #include "hybridslider.h"
 #include "ui_hybridslider.h"
 
-HybridSlider::HybridSlider(QWidget *parent) :
+HybridSlider::HybridSlider(QWidget *parent,
+                           int initVal,
+                           int rangeMin,
+                           int rangeMax) :
     QWidget(parent),
     ui(new Ui::HybridSlider)
 {
+    this->initVal = initVal;
+    this->rangeMin = rangeMin;
+    this->rangeMax = rangeMax;
+
     ui->setupUi(this);
+
+    ui->sliderVal->setValue(initVal);
+    valueChanged(initVal);
+    ui->sliderVal->setMinimum(rangeMin);
+    ui->sliderVal->setMaximum(rangeMax);
 
     ui->buttonEditApply->setText("Edit Range");
 
@@ -16,17 +28,46 @@ HybridSlider::HybridSlider(QWidget *parent) :
 
     ui->gridLayout->setSizeConstraint(QLayout::SetFixedSize);
 
-    connect(ui->buttonEditApply,SIGNAL(released()),this,SLOT(showRangeBox()));
+    connect(ui->sliderVal, SIGNAL(valueChanged(int)),
+            this, SLOT(valueChanged(int)));
+    connect(ui->buttonEditApply, &QPushButton::released, this,
+            [=]() {
+        if(currentMode == EDIT || currentMode == APPLIED)
+            showRangeBox();
+        emit editApplyClicked();
+    });
 }
 
 void HybridSlider::showRangeBox()
 {
-    ui->label_5->setVisible(true);
-    ui->lineEditMinVal->setVisible(true);
-    ui->label_6->setVisible(true);
-    ui->lineEditMaxVal->setVisible(true);
+    bool setRangeBoxVisibility;
 
-    ui->buttonEditApply->setText("Apply Range");
+    if(currentMode == APPLIED)
+    {
+        setRangeBoxVisibility = true;
+        currentMode = EDIT;
+    }
+    else
+    {
+        setRangeBoxVisibility = false;
+        currentMode = APPLIED;
+        // TODO: Apply Range values here
+    }
+
+    ui->label_5->setVisible(setRangeBoxVisibility);
+    ui->lineEditMinVal->setVisible(setRangeBoxVisibility);
+    ui->label_6->setVisible(setRangeBoxVisibility);
+    ui->lineEditMaxVal->setVisible(setRangeBoxVisibility);
+}
+
+void HybridSlider::applyValues()
+{
+    currentMode = APPLIED;
+}
+
+void HybridSlider::valueChanged(int value)
+{
+    ui->editValue->setText(QString::number(value));
 }
 
 HybridSlider::~HybridSlider()
--- hybridslider.h
+++ hybridslider.h
@@ -12,14 +12,32 @@ class HybridSlider : public QWidget
     Q_OBJECT
 
 public:
-    explicit HybridSlider(QWidget *parent = nullptr);
+    explicit HybridSlider(QWidget *parent = nullptr,
+                          int initVal = 0,
+                          int rangeMin = 0,
+                          int rangeMax = 100);
     ~HybridSlider();
 
 private slots:
     void showRangeBox();
+    void applyValues();
+    void valueChanged(int);
+
+signals:
+    void editApplyClicked();
 
 private:
     Ui::HybridSlider *ui;
+
+    enum MODE{
+        EDIT, APPLIED, FORCE_VALUE
+    };
+
+    MODE currentMode = APPLIED;
+
+    int initVal;
+    int rangeMin;
+    int rangeMax;
 };
 
 #endif // HYBRIDSLIDER_H
