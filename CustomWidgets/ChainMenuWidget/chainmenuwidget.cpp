6831c68a27b62dc9a84c8235366366d933001cb0
JairajJangle <jairaj.jangle@gmail.com>
2020-06-06 23:17:01 +0530

    1. Added Add/Remove button enable public functions 2. Added Add/Remove button signals to eb used in parent widget

--- CustomWidgets/ChainMenuWidget/chainmenuwidget.cpp
+++ CustomWidgets/ChainMenuWidget/chainmenuwidget.cpp
@@ -1,11 +1,36 @@
 #include "chainmenuwidget.h"
 #include "ui_chainmenuwidget.h"
 
-ChainMenuWidget::ChainMenuWidget(QWidget *parent) :
+ChainMenuWidget::ChainMenuWidget(QWidget *parent,
+                                 QList<QPair<int, QString>> chainMenuOpList) :
     QWidget(parent),
     ui(new Ui::ChainMenuWidget)
 {
     ui->setupUi(this);
+
+    for(QPair<int, QString> chainMenuItem : chainMenuOpList)
+    {
+        ui->comboBox->addItem(chainMenuItem.second, chainMenuItem.first);
+    }
+
+    connect(ui->buttonAdd, &QPushButton::released,this,
+            [=](){
+        emit addOperationClicked();
+    });
+
+    connect(ui->buttonRemove, &QPushButton::released,this,
+            [=](){
+        emit removeOperationClicked();
+    });
+}
+
+void ChainMenuWidget::setAddButonEnabled(bool enabled)
+{
+    ui->buttonAdd->setEnabled(enabled);
+}
+void ChainMenuWidget::setRemoveButtonEnabled(bool enabled)
+{
+    ui->buttonRemove->setEnabled(enabled);
 }
 
 ChainMenuWidget::~ChainMenuWidget()
--- CustomWidgets/ChainMenuWidget/chainmenuwidget.h
+++ CustomWidgets/ChainMenuWidget/chainmenuwidget.h
@@ -2,6 +2,7 @@
 #define CHAINMENUWIDGET_H
 
 #include <QWidget>
+#include <QPair>
 
 namespace Ui {
 class ChainMenuWidget;
@@ -12,8 +13,19 @@ class ChainMenuWidget : public QWidget
     Q_OBJECT
 
 public:
-    explicit ChainMenuWidget(QWidget *parent = nullptr);
+    explicit ChainMenuWidget(QWidget *parent = nullptr,
+                             QList<QPair<int, QString>> chainMenuOpList =
+            QList<QPair<int, QString>>() << QPair<int, QString>(0 ,""));
+
+    void setAddButonEnabled(bool enabled);
+    void setRemoveButtonEnabled(bool enabled);
+
     ~ChainMenuWidget();
+
+signals:
+    void addOperationClicked();
+    void removeOperationClicked();
+    void operationChanged(int chainMenuOpListIndex); // or OpCode
 
 private:
     Ui::ChainMenuWidget *ui;
