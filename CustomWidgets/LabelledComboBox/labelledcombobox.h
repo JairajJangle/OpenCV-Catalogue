97191e9a2bd6d589a4d14f6dbe56475b5adfc439
JairajJangle <jairaj.jangle@gmail.com>
2020-07-08 21:47:41 +0530

    Fixed width of title label and combobox

--- CustomWidgets/LabelledComboBox/labelledcombobox.h
+++ CustomWidgets/LabelledComboBox/labelledcombobox.h
@@ -1,3 +1,23 @@
+/* OpenCV Catalogue is a desktop GUI application to showcase some OpenCV functions
+ * and parameter adjustment using controls in GUI.
+ * Copyright (C) 2020  Jairaj Jangle
+ *
+ * This file is part of OpenCV Catalogue.
+ *
+ * OpenCV Catalogue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCV Catalogue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenCV Catalogue.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <QLabel>
@@ -16,14 +36,14 @@ public:
     QComboBox* comboBox = new QComboBox();
     explicit LabelledComboBox(QString title,
                               QMap<QString, QVariant> nameValueMap,
-                              int comboBoxFixedWidth = 100,
-                              int labelFixedWidth = 100,
+                              int comboBoxFixedWidth = 0,
+                              int labelFixedWidth = 0,
                               QWidget *parent = nullptr)
         :QHBoxLayout(parent)
     {
         titleLabel->setText(title);
-        titleLabel->setFixedWidth(labelFixedWidth);
-        titleLabel->setAlignment(Qt::AlignCenter);
+        if(labelFixedWidth != 0)
+            titleLabel->setFixedWidth(labelFixedWidth);
 
         QMapIterator<QString, QVariant> i(nameValueMap);
         while (i.hasNext())
@@ -32,7 +52,8 @@ public:
             comboBox->addItem(i.key(), i.value());
         }
 
-        comboBox->setFixedWidth(comboBoxFixedWidth);
+        if(comboBoxFixedWidth != 0)
+            comboBox->setFixedWidth(comboBoxFixedWidth);
 
         this->addWidget(titleLabel);
 
