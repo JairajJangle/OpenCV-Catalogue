ab5b266655ad4f8360f886b98c84c6507d6faaa6
JairajJangle <jairaj.jangle@gmail.com>
2020-09-06 21:56:07 +0530

    Separated source and header implementation for BackgroundSubtraction

--- OpenCVFunction/backgroundsubtract.h
+++ OpenCVFunction/backgroundsubtract.h
@@ -38,99 +38,26 @@ class BackgroundSubtraction : public BaseConfigWidget
 {
     Q_OBJECT
 public:
-    BackgroundSubtraction()
-    {
-        operationName = "Background Subtraction";
-        moreInfoLink = "https://docs.opencv.org/3.4/d7/df6/classcv_1_1BackgroundSubtractor.html";
-        initWidget();
-    }
-
-    cv::Mat getProcessedImage(cv::Mat inputImage) override try
-    {
-        cv::Mat outputImage;
-
-        bkgSubTechList.at(selectedTech)->apply(inputImage, outputImage, learningRate);
-
-        cv::Mat element = getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(1, 1));
-        morphologyEx(outputImage, outputImage, cv::MORPH_OPEN, element);
-
-        return outputImage;
-    }
-    catch(cv::Exception& e){
-        throw e;
-    } catch(std::exception& e) {
-        throw e;
-    }
-    catch(...){
-    throw std::string("Unknown Exception in ")
-    + std::string(typeid(this).name());
-}
+    BackgroundSubtraction();
+    cv::Mat getProcessedImage(cv::Mat inputImage) override;
 
 private slots:
-void bkgSubTechChanged(int bkgSubTech){
-    selectedTech = bkgSubTech;
-}
-
-void resetAnchorClicked(){
-    begin = cv::Point(-1, -1);
-}
+    void bkgSubTechChanged(int bkgSubTech);
+    void resetAnchorClicked();
 
 private:
-QList<cv::Ptr< cv::BackgroundSubtractor>> bkgSubTechList;
+    void initWidget() override;
 
-/*
- * More at: https://docs.opencv.org/3.4/d7/df6/classcv_1_1BackgroundSubtractor.html
- */
-QList<QString> bkgSubTechs = { "KNN", "MOG", "MOG2", "GMG", "GSOC", "CNT", "LSBP" };
+    QList<cv::Ptr< cv::BackgroundSubtractor>> bkgSubTechList;
 
-int selectedTech = 0; // KNN
+    // More at: https://docs.opencv.org/3.4/d7/df6/classcv_1_1BackgroundSubtractor.html
+    QList<QString> bkgSubTechs = { "KNN", "MOG", "MOG2", "GMG", "GSOC", "CNT", "LSBP" };
 
-double learningRate = 0.1;
-LineEditLayout* learningRateEditLayout  = new LineEditLayout("Learning Rate\n[0-1]",
-                                                             learningRate,
-                                                             150,
-                                                             150);
-
-ApplyResetButtonLayout* applyResetBox = new ApplyResetButtonLayout();
-
-void initWidget() override
-{
-    connect(applyResetBox, &ApplyResetButtonLayout::applyClicked,
-            this, [=](){ learningRate = learningRateEditLayout->getText().toDouble(); });
-    connect(applyResetBox, &ApplyResetButtonLayout::resetClicked,
-            this, [=](){
-        learningRate = 0.1;
-        learningRateEditLayout->setText(QString::number(learningRate));
-    });
-
-    learningRateEditLayout->lineEdit->setValidator(
-                new QRegExpValidator(QRegExp(RegExps::decimal0To1)));
-
-    bkgSubTechList.append(cv::createBackgroundSubtractorKNN(1,2000.0,false)); // KNN
-    bkgSubTechList.append(cv::bgsegm::createBackgroundSubtractorMOG()); // MOG
-    bkgSubTechList.append(cv::createBackgroundSubtractorMOG2()); // MOG2
-    bkgSubTechList.append(cv::bgsegm::createBackgroundSubtractorGMG()); // GMG
-    bkgSubTechList.append(cv::bgsegm::createBackgroundSubtractorGSOC()); // GSOC
-    bkgSubTechList.append(cv::bgsegm::createBackgroundSubtractorCNT()); // CNT
-    bkgSubTechList.append(cv::bgsegm::createBackgroundSubtractorLSBP()); // LSBP
-
-    for(int ibkgSubTech = 0; ibkgSubTech < bkgSubTechs.size(); ibkgSubTech++)
-    {
-        QRadioButton *radioButton =
-                new QRadioButton(bkgSubTechs[ibkgSubTech]);
-        if(ibkgSubTech == 0)
-            radioButton->setChecked(true);
-
-        vBoxSub->addWidget(radioButton);
-
-        connect(radioButton, &QRadioButton::clicked, this,
-                [=]() {
-            selectedTech = ibkgSubTech;
-        });
-    }
-    vBoxSub->addLayout(learningRateEditLayout);
-    vBoxSub->addLayout(applyResetBox);
-
-    BaseConfigWidget::initWidget();
-}
+    int selectedTech = 0; // KNN
+    double learningRate = 0.1;
+    LineEditLayout* learningRateEditLayout  = new LineEditLayout("Learning Rate\n[0-1]",
+                                                                 learningRate,
+                                                                 150,
+                                                                 150);
+    ApplyResetButtonLayout* applyResetBox = new ApplyResetButtonLayout();
 };
