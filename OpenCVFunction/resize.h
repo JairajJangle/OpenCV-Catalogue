f31c55daf5e632a885dc7d7730eb6e95ba4a9900
JairajJangle <jairaj.jangle@gmail.com>
2020-09-09 08:48:53 +0530

    Added Minimal structure to Resize operation widget

--- OpenCVFunction/resize.h
+++ OpenCVFunction/resize.h
@@ -1,4 +1,55 @@
-#ifndef RESIZE_H
-#define RESIZE_H
+/* OpenCV Catalogue is a desktop GUI application to showcase some OpenCV functions
+ * and parameter adjustment using controls in GUI.
+ * Copyright (C) 2020  Jairaj Jangle
+ *
+ * This file is part of OpenCV Catalogue.
+ *
+ * OpenCV Catalogue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCV Catalogue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenCV Catalogue.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-#endif // RESIZE_H
+#pragma once
+
+#include "CustomWidgets/baseconfigwidget.h"
+
+class Resize : public BaseConfigWidget
+{
+    Q_OBJECT
+public:
+    Resize()
+    {
+        operationName = "Resize";
+        moreInfoLink = "https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html?highlight=resize#resize";
+        this->initWidget();
+    }
+
+    cv::Mat getProcessedImage(cv::Mat inputImage) override try
+    {
+        return inputImage;
+    }
+    catch(cv::Exception& e){
+        throw e;
+    } catch(std::exception& e) {
+        throw e;
+    }
+    catch(...){
+    throw std::string("Unknown Exception in ")
+    + std::string(typeid(this).name());
+}
+
+private:
+void initWidget() override
+{
+    BaseConfigWidget::initWidget();
+}
+};
